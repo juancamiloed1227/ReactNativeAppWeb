b13617bf85c6eff1a68e9630a887ce9f04850675
LinaAgudeloGarces <linagub70@gmail.com>
2022-05-11 18:58:24 -0500

    Proyecto react

---
+++ proyectoFinal/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        La mejor App del mundo Hola mundo estamos felices feliz feliz de que los
+        tengo
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
