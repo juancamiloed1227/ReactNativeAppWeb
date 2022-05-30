a07bf01c3c9f21c8429f2e39a329af7c6ac7ef12
Majo2000 <mariajota00@gmail.com>
2022-05-30 15:19:27 -0500

    Pantalla final tickets
    pantalla final tickets

--- proyectoFinal/src/screens/Tickets.js
+++ proyectoFinal/src/screens/Tickets.js
@@ -1,81 +1,102 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import RNPickerSelect from "react-native-picker-select";
+import { StyleSheet, Text, View, Alert,TouchableOpacity, Image} from "react-native";
 import NumericInput from 'react-native-numeric-input';
 
-const tipoBoletas =  ["Basic Pass Legends Fest", "Full Pass Legends Fest"]
-
+const tipoBoletas = ["Basic Pass Legends Fest", "Full Pass Legends Fest"]
 
 export default function Tickets() {
   return (
-     
-      <View style={styles.container}>
-       <View style={styles.row}>
-      <View style={[styles.box]}>
-          <Text style={styles.tituloT}>Tipo de boleta</Text>
-          <RNPickerSelect 
-            placeholder={{ label: "Seleccionar", value: null }}
-                 onValueChange={(value) => console.log(value)}
-                 items={[
-                     { label: "Basic Pass Legends Fest", value: "" },
-                     { label: "Full Pass Legends Fest", value: "" }
-                 ]}
-             />  
+  
+   <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <Text style={styles.tituloT}>Tipo de boleta</Text>
+      <View style={{ backgroundColor: "#B277FF", padding: 10 ,width: 375, height: 200}}>
+        <View style={{ flexDirection: "row",backgroundColor: "#8143D1", width: 350, height: 85, alignItems:"center", justifyContent:"center", borderRadius:6, padding: 6.5, marginLeft:3.2,justifyContent:'space-between', marginBottom: 10}}>
+          <Image style={{width: 30, height: 30}}
+          source={{uri: 'https://cdn-icons-png.flaticon.com/512/3702/3702886.png'}} />
+          <Text style={styles.titulo}>Basic Pass Legends Fest</Text>
+          <NumericInput type='up-down' onChange={value => console.log(value)} totalWidth={100} iconStyle={{ color: '#240252' }}minValue={0} />
         </View>
-        <View style={[styles.box]}>
-      <Text style={styles.tituloT}>Cantidad</Text>
-          <NumericInput type='up-down' onChange={value => console.log(value)} totalWidth={100} iconStyle={{ color: '#6495ED' }}minValue={0} />
-          </View>
+        
+        <View style={{ flexDirection: "row",backgroundColor: "#8143D1", padding: 10, width: 350, height: 85, alignItems:"center", justifyContent:"center", borderRadius:6, padding: 6.5, marginLeft:3.2,justifyContent:'space-between', marginBottom: 10}}>
+          <Image style={{width: 34, height: 34}}
+          source={{uri: 'https://cdn-icons-png.flaticon.com/512/1061/1061702.png'}}/>
+          <Text style={styles.titulo}>Full Pass Legends Fest   </Text>
+          <NumericInput type='up-down' onChange={value => console.log(value)} totalWidth={100} iconStyle={{ color: '#240252' }}minValue={0} />
+        </View>
+      </View>
+
+        <View style={{alignItems: "center",padding: 20}}>
+          <TouchableOpacity onPress={() => Alert.alert('Se calculó el total de la compra')}>
+            <View style={{
+              backgroundColor: '#3B0285',
+              alignItems: 'center', 
+              justifyContent: 'center',
+              borderRadius: 10,
+              width:150 ,
+              padding: 10
+          }}>
+            <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>Calcular</Text>
+            </View>
+        </TouchableOpacity>
+        
+      </View>
+      <View style={{ backgroundColor: "#8143D1", borderColor: "#8143D1",borderRadius: 10,borderWidth: 3, padding: 15}} >
+        <Text style={{ color: 'white', fontSize: 19, fontWeight: 'bold', alignItems: "center" , justifyContent: "center", marginLeft: 50}}>Resumen de la compra</Text>
+        <View style={{ flexDirection: "row", width: 350, height: 50, padding: 6}}>
+       <Image style={{ width: 30, height: 30, padding: 10, justifyContent: "flex-start" }}
+            source={{ uri: 'https://cdn-icons-png.flaticon.com/512/3702/3702886.png' }} />
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center" , justifyContent: "flex-start", padding: 10}}>Basic Pass Legends Fest</Text>
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center" , justifyContent: "flex-end", padding: 10}}>$</Text>
+        </View>
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center", justifyContent: "flex-start", padding: 10 }}>Cantidad:</Text>
+        <View style={{ flexDirection: "row", width: 350, height: 50, padding: 6}}>
+       <Image style={{ width: 30, height: 30, padding: 10, justifyContent: "flex-start" }}
+            source={{ uri: 'https://cdn-icons-png.flaticon.com/512/1061/1061702.png' }} />
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center" , justifyContent: "flex-start", padding: 10}}>Full Pass Legends Fest   </Text>
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center" , justifyContent: "flex-end", padding: 10}}>$</Text>
+        </View>
+        <Text style={{ color: 'white', fontSize: 13, alignItems: "center" , justifyContent: "flex-start", padding: 10}}>Cantidad:</Text>
+      </View >
+
+      <View style={{alignItems: "center",padding: 20}}>
+          <TouchableOpacity onPress={() => Alert.alert('La compra se ha realizado con éxito')}>
+            <View style={{
+              backgroundColor: '#3B0285',
+              alignItems: 'center', 
+              justifyContent: 'center',
+              borderRadius: 10,
+              width:210 ,
+              padding: 10
+          }}>
+            <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>Comprar ahora</Text>
+            </View>
+        </TouchableOpacity>
+      </View >
+      <View style={{ backgroundColor: "#B277FF", padding: 15 ,width: 375, height: 50}}>
+        <Text style={{alignContent: "center", justifyContent: "center", color: "#240252", marginLeft:110, fontSize:11}}>Terminos y condiciones</Text>
+        <Text style={{alignContent: "center", justifyContent: "center", color: "#240252", marginLeft:84, fontSize:11}}>Copyright&copy; Legends Fest - 2022</Text>
       </View>
       </View>
-  );
+  ); 
 }
 const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    height: 200,
-    width: 200,
-    padding:20,
-  },
-   row: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10
-  },
-container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start', //replace with flex-end or center
-    borderBottomWidth: 1,
-    borderBottomColor: '#000'
-  },
-  separadorH: {
-    width: 0.1,
-  },
-
-  tituloT: {
-    color: "black",
-    fontSize: 20,
-    marginRight: 5,
-  },
-
-  title: {
-    height: 60,
-    width: 200,
-    marginLeft: 5,
-    padding: 0,
-  },
-
-  /*Para el flex*/
   container: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'baseline',
-    },
-    box: {
-      height: 200,
-      width: 200,
-
-    }
+    flex: 1,
+    padding: 20,
+  },
+   tituloT: {
+    color: "#240252",
+    fontSize: 20,
+     marginRight: 5,
+    marginBottom:12
+  },
+   titulo: {
+    color: "white",
+    fontSize: 15,
+     marginRight: 5,
+    padding: 10
+  },
 });
